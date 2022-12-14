10f49be0d8322bda11f1ae8f6f72f0e791ff48c5
vedran-sabic <vedran.sabic@skole.hr>
2022-12-14 20:01:24 +0100

    druga verzija s arrayevima

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 /* 
 1. pripremamo se za brojanje klikova na buttonu - radimo varijablu counter
-2. onclick event listenerom spajamo JS funkciju na HTML button
-3. spajamo se na h2 element, da se tu vidi vrijednost brojača
+2. dodajemo onclick event listenere na HTML buttone, 
+    tako omogućavamo da se pozove JS funkcija klikom na svaki button
+3. deklariramo funkcije koje će se izvršiti svakim klikom na te buttone
+4. spajamo se na h2 element, da tu prikažemo vrijednost brojača
+5. spajamo se na h3 element, da tu prikažemo spremljene vrijednosti
+6. refaktoriramo sve koristeći nizove
+7. dodajemo event listener na reset button
+8. deklariramo funkciju koja će se izvršiti klikom na reset button
 */
 
 let counter = 0;
-
 // Document Object Model - pristupamo HTML elementima h2 i h3
 let numberOfPassengers = document.getElementById("number-of-passengers");
 let numberOfRides = document.getElementById("number-of-rides");
+// stvaramo prazan array
+let todaysRides = [];
 
 function add() {
     counter += 1;
@@ -16,8 +23,17 @@ function add() {
 }
 
 function save() {
-    let counterString = counter + " - ";
-    numberOfRides.innerHTML += counterString;
+    // push() dodaje novi član na kraj niza 
+    todaysRides.push(counter);
+    // join() uzima član po član arraya i odvaja ih načinom navedenim u navodnicima, pa od svega toga radi string
+    numberOfRides.textContent = `Današnje vožnje (broj putnika): ${todaysRides.join(' - ') || 0}`;
     counter = 0;
-    numberOfPassengers.innerHTML = 0;
+    numberOfPassengers.textContent = counter;
+}
+
+function reset() {
+    counter = 0;
+    todaysRides = [];
+    numberOfRides.textContent = `Današnje vožnje (broj putnika): ${todaysRides.join(' - ') || 0}`;
+    numberOfPassengers.textContent = counter;
 }
